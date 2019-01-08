9fe9386566b3654bdf26848a6ca914f7a2780832
Sandro <sandro.ferreira@gruporbs.com.br>
2019-01-08 14:00:07 -0200

    Modificacoes no database

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -10,8 +10,8 @@ class Course(models.Model):
     start_date = models.DateField(
         'Data de Início', null=True, blank=True
     )
-    image = models.ImageField(upload_to='courses/images',
-                              verbose_name='Imagem') #Caminho onde o arquivo será salvo
+    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True ) #Caminho onde o arquivo será salvo
+    #Blank = True quer dizer que não é um campo obrigatorio.
 
     created_at = models.DateTimeField('Criado em', auto_now_add=True) #Toda vez que eu criar um curso, automaticamente será a data atual
     updated_at = models.DateTimeField('Atualizado em', auto_now=True) #Toda vez que eu salvar um curso, automaticamente será a data atual
